fdc2de15a65631788d9d783ecbc4187f8b4e1dc7
JosephMar808 <jmaradiaga901@gmail.com>
2022-10-30 18:53:22 -0400

    Working on execute test

--- src/DecodeStage.C
+++ src/DecodeStage.C
@@ -3,13 +3,17 @@
 #include <stdio.h>
 #include "Memory.h"
 #include "ConditionCodes.h"
-#include "PipeReg.h"
-#include "PipeRegField.h"
+#include "Instructions.h"
 #include "RegisterFile.h"
+#include "PipeRegField.h"
+#include "PipeReg.h"
 #include "D.h"
+#include "E.h"
 #include "Stage.h"
 #include "DecodeStage.h"
+#include "Status.h"
 #include "Debug.h"
+
 
 bool DecodeStage::doClockLow(PipeReg ** pregs) {
 	return false;	
