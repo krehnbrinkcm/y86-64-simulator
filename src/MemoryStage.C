2958d1219314d359decef94f698d2ec11ba2ad0e
Cole <krehnbrinkcm@appstate.edu>
2022-10-30 18:59:47 -0400

    Memory and Writeback .C

--- src/MemoryStage.C
+++ src/MemoryStage.C
@@ -1,12 +1,26 @@
+#include <iostream>
+#include <cstdint>
+#include <stdio.h>
+#include "Memory.h"
+#include "ConditionCodes.h"
+#include "Instructions.h"
+#include "RegisterFile.h"
+#include "PipeRegField.h"
+#include "PipeReg.h"
+#include "M.h"
+#include "W.h"
 #include "Stage.h"
 #include "MemoryStage.h"
+#include "Status.h"
+#include "Debug.h"
+
 
 
 bool MemoryStage::doClockLow(PipeReg ** pregs) {
 	
 } 
 
-bool MemoryStage::doClockLow(PipeReg ** pregs) {
+void MemoryStage::doClockLow(PipeReg ** pregs) {
 
 }
 
--- src/WritebackStage.C
+++ src/WritebackStage.C
@@ -1,12 +1,25 @@
+#include <iostream>
+#include <cstdint>
+#include <stdio.h>
+#include "Memory.h"
+#include "ConditionCodes.h"
+#include "Instructions.h"
+#include "RegisterFile.h"
+#include "PipeRegField.h"
+#include "PipeReg.h"
+#include "W.h"
 #include "Stage.h"
 #include "WritebackStage.h"
+#include "Status.h"
+#include "Debug.h"
+
 
 
 bool WritebackStage::doClockLow(PipeReg ** pregs) {
 	
 } 
 
-bool WritebackStage::doClockLow(PipeReg ** pregs) {
+void WritebackStage::doClockLow(PipeReg ** pregs) {
 
 }
 
