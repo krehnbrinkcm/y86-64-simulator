9f2cb4fcc1ec9c11cee814540611ba8dfcc9c9d0
JosephMar808 <jmaradiaga901@gmail.com>
2022-10-30 18:42:27 -0400

    Working on decodestage test

--- src/DecodeStage.C
+++ src/DecodeStage.C
@@ -1,12 +1,21 @@
+#include <iostream>
+#include <cstdint>
+#include <stdio.h>
+#include "Memory.h"
+#include "ConditionCodes.h"
+#include "PipeReg.h"
+#include "PipeRegField.h"
+#include "RegisterFile.h"
+#include "D.h"
 #include "Stage.h"
 #include "DecodeStage.h"
-
+#include "Debug.h"
 
 bool DecodeStage::doClockLow(PipeReg ** pregs) {
-	
+	return false;	
 } 
 
-bool DecodeStage::doClockLow(PipeReg ** pregs) {
-
+void DecodeStage::doClockHigh(PipeReg ** pregs) {
+	
 }
 
