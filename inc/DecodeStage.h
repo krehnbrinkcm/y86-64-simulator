9a44e566afcb16a7104e43a5c7b07d8808237f65
Cole <krehnbrinkcm@appstate.edu>
2022-10-30 20:42:48 -0400

    Sunday progress

--- inc/DecodeStage.h
+++ inc/DecodeStage.h
@@ -1,6 +1,10 @@
 class DecodeStage: public Stage
 {
 
+private: 
+      void setEInput(PipeReg * dreg, uint64_t e_stat, uint64_t e_icode, uint64_t e_ifun, uint64_t e_valc, uint64_t e_vala,
+uint64_t e_valb, uint64_t e_dste, uint64_t e_dstm, uint64_t e_srca, uint64_t e_srcb);
+
 public: 
       bool doClockLow(PipeReg ** pregs);
       void doClockHigh(PipeReg ** pregs);
--- src/DecodeStage.C
+++ src/DecodeStage.C
@@ -15,11 +15,43 @@
 #include "Debug.h"
 
 
+
 bool DecodeStage::doClockLow(PipeReg ** pregs) {
-	return false;	
+	PipeReg * dreg = pregs[DREG];
+	PipeReg * ereg = pregs[EREG];
+
+	bool mem_error = false;
+
+	uint64_t stat = dreg->get(D_STAT);
+	uint64_t icode = dreg->get(D_ICODE);
+	uint64_t ifun = dreg->get(D_IFUN);
+	uint64_t ra = dreg->get(D_RA);
+	uint64_t rb = dreg->get(D_RB);
+	uint64_t valb = dreg->get(D_VALB);
+	uint64_t valp = dreg->get(D_VALP);
+
+	setEInput(ereg, stat, icode, ifun, 0, 0, valb, );	
+	return false;
 } 
 
 void DecodeStage::doClockHigh(PipeReg ** pregs) {
-	
+	PipeReg * dreg = pregs[DREG];
+	PipeReg * ereg = pregs[EREG];
+	dreg->normal();
+	ereg->normal();
+}
+
+void DecodeStage::setEInput(PipeReg * ereg, uint64_t e_stat, uint64_t e_icode, uint64_t e_ifun, uint64_t e_valc, uint64_t e_vala,
+uint64_t e_valb, uint64_t e_dste, uint64_t e_dstm, uint64_t e_srca, uint64_t e_srcb) {
+	ereg->set(E_STAT, e_stat);
+	ereg->set(E_ICODE, e_icode);
+	ereg->set(E_IFUN, e_ifun);
+	ereg->set(E_VALC, e_valc);
+	ereg->set(E_VALA, e_vala);
+	ereg->set(E_VALB, e_valb);
+	ereg->set(E_DSTE, e_dste);
+	ereg->set(E_DSTM, e_dstm);
+	ereg->set(E_SRCA, e_srca);
+	ereg->set(E_SRCB, e_srcb);
 }
 
